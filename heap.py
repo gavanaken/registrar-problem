7f9c1eaa538d3b881ef9e9656c24e35151d0bf79
Conor <cstuartroe@haverford.edu>
2018-10-27 12:44:23 -0400

    made heap autobalancing

--- heap.py
+++ heap.py
@@ -15,7 +15,7 @@ class MinBinHeap:
         return out
 
     def __str__(self):
-        lines = self.root.lines()
+        lines = self.root.lines() if self.root else [""]
         return("\n".join(lines))
 
 class MinBinNode:
@@ -23,6 +23,7 @@ class MinBinNode:
         self.val = _val
         self.right = None
         self.left = None
+        self.balance = 0
 
     def push(self,newval):
         if self.val < newval:
@@ -33,25 +34,33 @@ class MinBinNode:
 
         if self.left is None:
             self.left = MinBinNode(to_push)
+            self.balance -= 1
         elif self.right is None:
             self.right = MinBinNode(to_push)
-        elif self.left.val > self.right.val:
+            self.balance += 1
+        elif self.balance >= 0:
             self.left.push(to_push)
+            self.balance -= 1
         else:
             self.right.push(to_push)
+            self.balance += 1
 
     def pop(self):
         out = self.val
         if self.left is None and self.right is None:
             return out, None
         elif self.left is None:
-            self.val, self.left = self.left.pop()
-        elif self.right is None:
             self.val, self.right = self.right.pop()
+            self.balance -= 1
+        elif self.right is None:
+            self.val, self.left = self.left.pop()
+            self.balance += 1
         elif self.left.val < self.right.val:
             self.val, self.left = self.left.pop()
+            self.balance += 1
         else:
             self.val, self.right = self.right.pop()
+            self.balance -= 1
         return out, self
 
     def lines(self):
